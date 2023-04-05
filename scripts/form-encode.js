e9cd4a9532843a6d2eb1252bc8783f3c0d65e4f4
Diego Diaz Rodas <diegodiazrodas@gmail.com>
2023-04-05 16:53:46 -0300

    finalizado de proyecto, envio de mails con emailjs

--- scripts/form-encode.js
+++ scripts/form-encode.js
@@ -1,72 +1,66 @@
-// const form = document.querySelector('#form');
-// form.addEventListener('submit', e => {
-//   e.preventDefault();
-//   const data = new FormData(form);
-//   const xhr = new XMLHttpRequest();
-//   xhr.open('POST', 'diegodiazrodas@gmail.com');
-//   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-//   xhr.send(new URLSearchParams(data).toString());
-//   console.log(xhr.send(new URLSearchParams(data).toString()))
-// });
+let formularioContacto = document.getElementById("contactForm");
+let clientName = document.getElementById("clientName");
+let clientEmail = document.getElementById("clientEmail");
+let clientSubject = document.getElementById("clientSubject");
+let clientBody = document.getElementById("bodyText");
 
-let formularioContacto = document.getElementById("formulario");
-let consultas = [];
-let idConsulta = 1;
+function alerts(bodyAlert) {
+  console.log(bodyAlert);
 
-const traerConsultasDeLS = () => {
-  if (localStorage.getItem("consultas")) {
-    consultas = JSON.parse(localStorage.getItem("consultas")); 
-    consultas.forEach(() => idConsulta++); 
-  }
-};
+  Swal.fire({
+    position: "center",
+    icon: bodyAlert.icon,
+    title: bodyAlert.title,
+    text: `${bodyAlert.text}: ${bodyAlert.name}`,
+    showConfirmButton: false,
+    timer: 5000,
+  });
+}
 
-traerConsultasDeLS();
-
-const crearNuevaConsulta = () => {
-  consultas = JSON.parse(localStorage.getItem("consultas")) || [];
-
-  let consulta = {
-    nombre: document.getElementById("nombreCliente").value,
-    // apellido: document.getElementById("apellidoCliente").value,
-    email: document.getElementById("emailCliente").value,
-    asunto: document.getElementById("asuntoCliente").value,
-    // actividad: document.getElementById("actividad").value,
-    texto: document.getElementById("textoConsultas").value,
-    id: idConsulta,
+const sendEmail = (bodyAlert) => {
+  let params = {
+    name: clientName.value,
+    email: clientEmail.value,
+    subject: clientSubject.value,
+    message: clientBody.value,
   };
 
-  idConsulta++;
+  let serviceID = "service_zte3jwt";
+  let templateID = "template_avh19j4";
 
-  consultas.push(consulta);
-};
-
-guardarFormularioLocalStorage = (clave, valor) => {
-  localStorage.setItem(clave, valor);
+  emailjs
+    .send(serviceID, templateID, params)
+    .then(function (response) {
+      alerts(bodyAlert);
+      //console.log(JSON.stringify(response));
+    })
+    .catch(function (error) {
+      alert("Error" + JSON.stringify(error));
+    });
 };
 
 formularioContacto.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  let bodyAlert = {
+    icon: "success",
+    title: "Consulta cargada correctamente",
+    text: "Muchas gracias",
+    name: clientName.value,
+  };
+
   try {
-    crearNuevaConsulta();
-    guardarFormularioLocalStorage("consultas", JSON.stringify(consultas));
-
-    let { nombre } = consultas.find(
-      (persona) =>
-        persona.nombre === nombreCliente.value
-    );
-
-    Swal.fire({
-      position: "center",
-      icon: "success",
-      title: "Consulta cargada correctamente",
-      text: `Muchas gracias: ${nombre}`,
-      showConfirmButton: false,
-      timer: 2500,
-    });
+    // Envía el correo electrónico utilizando EmailJS.
+    sendEmail(bodyAlert)
   } catch (e) {
-    console.log(e);
+    bodyAlert = {
+      icon: "error",
+      title: "No se pudo cargar su consulta",
+      text: "Disculpe las molestias, por favor contactese por mail a info@cenedu.org",
+      name: clientEmail.value,
+    };
+    alerts(bodyAlert);
+    //console.log(e);
   }
-
   formularioContacto.reset();
 });
